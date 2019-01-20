45dfa32f9ee0a3afd4e2685359558ddd6188d5c6
Matt Braun <mb.trailstrailstrails@gmail.com>
2019-01-20 13:04:08 +1100

    responsive design for parallax effect
    no parallax on mobile

--- js/title.js
+++ js/title.js
@@ -1,22 +1,24 @@
 var parallax = function() {
-    var nav = $('.nav'),
-        navHeight = nav.outerHeight(true),
-        clientWidth5 = $( document ).width() * 0.05; // equals 5vw
-    $(nav).parent().css('padding-top', navHeight);
-    $(window).scroll(function() {
-        var scrollOffset = $(window).scrollTop();   // pixels scrolled down
-        if (scrollOffset < navHeight) {             // reduce nav height by scroll offset
-            $(nav).css('height', (navHeight - scrollOffset));
-        }
-        else {                                      // important for return to page
-            $(nav).css('height', '5vw');
-        }
+    if (document.documentElement.clientWidth > 480) {
+        var nav = $('.nav'),
+            navHeight = nav.outerHeight(true),
+            clientWidth5 = $(document).width() * 0.05; // equals 5vw
+        $(nav).parent().css('padding-top', navHeight);
+        $(window).scroll(function () {
+            var scrollOffset = $(window).scrollTop();   // pixels scrolled down
+            if (scrollOffset < navHeight) {             // reduce nav height by scroll offset
+                $(nav).css('height', (navHeight - scrollOffset));
+            }
+            else {                                      // important for return to page
+                $(nav).css('height', '5vw');
+            }
 
-        if (scrollOffset > (navHeight - clientWidth5)) {     // if desired min size is reached
-            nav.addClass('fixme');                  // add black overlay
-        } else {
-            nav.removeClass('fixme');               // remove black overlay
-        }
-    });
+            if (scrollOffset > (navHeight - clientWidth5)) {     // if desired min size is reached
+                nav.addClass('fixme');                  // add black overlay
+            } else {
+                nav.removeClass('fixme');               // remove black overlay
+            }
+        });
+    }
 };
 parallax();
